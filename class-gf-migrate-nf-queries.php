de75f0f4fd01527c31c4b608a71725099f825afa
JeffMatson <jeff@jeffmatson.net>
2016-02-18 21:57:58 -0500

    Initial commit of query class

---
+++ class-gf-migrate-nf-queries.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Used for pulling Ninja Forms data
+ */
+
+class GF_Migrate_NF_Queries {
+
+	// Gets wp_nf_objectmeta table
+	public static function get_all_objectmeta() {
+
+	}
+
+	// Gets wp_nf_objects table
+	public static function get_all_objects() {
+
+	}
+
+	// Gets wp_nf_relationships table
+	public static function get_all_relationships() {
+
+	}
+
+	// Gets wp_ninja_forms_fav_fields table
+	public static function get_all_forms_fav_fields() {
+
+	}
+
+	// Gets wp_ninja_forms_fields table
+	public static function get_all_forms_fields() {
+
+	}
+
+}
