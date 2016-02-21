70dbdf07aaae5bfe73f833ca4a770e8d9dad2fd6
Travis Lopes <travis.lopes@me.com>
2016-02-21 00:54:36 -0500

    Added Migrate Ninja Forms tab

--- class-gf-migrate-nf.php
+++ class-gf-migrate-nf.php
@@ -34,83 +34,202 @@ class GF_Migrate_NF extends GFAddOn {
 	 * Register required files and filters.
 	 *
 	 * @access public
-	 * @return void
 	 */
 	public function init() {
-		
+
+		// Load needed classes.
 		require_once 'includes/class-gf-migrate-nf-api.php';
 		require_once 'includes/class-gf-migrate-nf-field.php';
+
+		// Add "Migrate Ninja Forms" tab to Migrate/Export page.
+		add_filter( 'gform_export_menu', array( $this, 'add_migrate_tab' ), 10, 1 );
+
+		// Render "Migrate Ninja Forms" tab.
+		add_action( 'gform_export_page_migrate_ninja_forms', array( $this, 'migrate_forms_page' ), 10, 0 );
 
 		parent::init();
 
 	}
 	
 	/**
-	 * Migrate Ninja Forms forms and submissions to Gravity Forms.
-	 *
+	 * Add "Migrate Ninja Forms" tab to Migrate/Export page.
+	 * 
+	 * @access public
+	 * @param  array $tabs - Tabs on the Migrate/Export page.
+	 * @return array $tabs
+	 */
+	public function add_migrate_tab( $tabs ) {
+		
+		// Add tab for Migrate Ninja Forms page.
+		$tabs['99'] = array(
+			'name'  => 'migrate_ninja_forms',
+			'label' => esc_html__( 'Migrate Ninja Forms', 'migrate-ninja-forms-to-gravity-forms' )
+		);
+		
+		return $tabs;
+		
+	}
+	
+	/**
+	 * Render "Migrate Ninja Forms" tab.
+	 * 
 	 * @access public
 	 */
-	public function migrate_forms() {
+	public function migrate_forms_page() {
 		
-		// Get Ninja Forms.
-		$ninja_forms = GF_Migrate_NF_API::get_forms();
-
-		// Convert and save each form.
-		foreach ( $ninja_forms as $ninja_form_id => $ninja_form ) {
-			
-			// Convert form.
-			$form = $this->convert_form( $ninja_form );
-			
-			// Save form and capture new ID.
-			$form_id = GFAPI::add_form( $form );
-			
-			// Convert submissions.
-			$entries = $this->convert_submissions( $ninja_form_id, $form_id );
+		$html = '';
 		
-			// Save entries.
-			GFAPI::add_entries( $entries, $form_id );
-			
+		// Handle form submission.
+		$this->maybe_migrate_forms();
+		
+		// Get all Ninja Forms forms.
+		$forms = GF_Migrate_NF_API::get_forms();
+		
+		// Display page header.
+		$page_title = esc_html__( 'Migrate Ninja Forms', 'migrate-ninja-forms-to-gravity-forms' );
+		GFExport::page_header( $page_title );
+		
+		// Add instructions.
+		$html .= '<p>' . esc_html__( 'Select the Ninja Forms forms you would like to migrate. When you click the Migrate button below, Gravity Forms will migrate the forms and their submissions.', 'migrate-ninja-forms-to-gravity-forms' ) . '</p>';
+		$html .= '<div class="hr-divider"></div>';
+		
+		// Start migrate form.
+		$html .= '<form id="gform_ninja_forms_migrate" method="post" style="margin-top:10px;">';
+		$html .= wp_nonce_field( 'gform_ninja_forms_migrate', 'gform_ninja_forms_migrate_nonce', true, false );
+		
+		// Open table.
+		$html .= '<table class="form-table">';
+		$html .= '<tr valign="top">';
+		$html .= '<th scope="row"><label for="migrate_forms">' . esc_html__( 'Select Forms', 'migrate-ninja-forms-to-gravity-forms' ) . '</label></th>';
+		
+		// Add forms.
+		$html .= '<td><ul>';
+		foreach ( $forms as $form_id => $form ) {
+			$html .= '<li>';
+			$html .= '<input type="checkbox" name="ninja_form_id[]" id="gf_form_id_' . esc_attr( $form_id ) . '" value="' . esc_attr( $form_id ) . '" />';
+			$html .= '<label for="gf_form_id_' . esc_attr( $form_id ) . '">' . $form['form_title'] . '</label>';
+			$html .= '</li>';
 		}
+		$html .= '</td></ul>';
+		
+		// Close table.
+		$html .= '</tr></table>';
+		
+		// Add submit button.
+		$html .= '<br /><br /><input type="submit" value="' . esc_attr__( 'Migrate Forms', 'migrate-ninja-forms-to-gravity-forms' ) . '" name="migrate_forms" class="button button-primary" />';
+		
+		// Close migrate form.
+		$html .= '</form>';
+		
+		// Display page contents.
+		echo $html;
+		
+		// Display page footer.
+		GFExport::page_footer();
 		
 	}
 
 	/**
-	 * Convert a Ninja Form to a Gravity Form.
+	 * Handle "Migrate Ninja Forms" form submission.
 	 * 
 	 * @access public
-	 * @param mixed $ninja_form
-	 * @return array $form
+	 */
+	public function maybe_migrate_forms() {
+		
+		// Check user permissions.
+		if ( ! GFCommon::current_user_can_any( 'gravityforms_edit_forms' ) ) {
+			wp_die( 'You do not have permission to access this page' );
+		}
+
+		// If form wasn't submitted, return.
+		if ( ! rgpost( 'migrate_forms' ) ) {
+			return;
+		}
+		
+		// Verify nonce.
+		check_admin_referer( 'gform_ninja_forms_migrate', 'gform_ninja_forms_migrate_nonce' );
+		
+		// If no forms were chosen to be migrated, return.
+		if ( empty( rgpost( 'ninja_form_id' ) ) ) {
+			return;
+		}
+		
+		// Migrate forms.
+		$this->migrate_forms( rgpost( 'ninja_form_id' ) );
+		
+		// Display success message.
+		$form_text = count( rgpost( 'ninja_form_id' ) ) > 1 ? __( 'forms', 'migrate-ninja-forms-to-gravity-forms' ) : __( 'form', 'migrate-ninja-forms-to-gravity-forms' );
+		GFCommon::add_message( sprintf( __( "Gravity Forms imported %d {$form_text} successfully.", 'migrate-ninja-forms-to-gravity-forms' ), count( rgpost( 'ninja_form_id' ) ) ) );
+		
+	}
+
+	/**
+	 * Migrate Ninja Forms forms and submissions to Gravity Forms.
+	 *
+	 * @access public
+	 * @param  array $form_ids - The Ninja Forms form IDs being migrated.
+	 */
+	public function migrate_forms( $form_ids = array() ) {
+
+		// Get Ninja Forms.
+		$ninja_forms = GF_Migrate_NF_API::get_forms( $form_ids );
+
+		// Convert and save each form.
+		foreach ( $ninja_forms as $ninja_form_id => $ninja_form ) {
+
+			// Convert form.
+			$form = $this->convert_form( $ninja_form );
+
+			// Save form and capture new ID.
+			$form_id = GFAPI::add_form( $form );
+
+			// Convert submissions.
+			$entries = $this->convert_submissions( $ninja_form_id, $form_id );
+
+			// Save entries.
+			GFAPI::add_entries( $entries, $form_id );
+
+		}
+
+	}
+
+	/**
+	 * Convert a Ninja Form to a Gravity Form.
+	 *
+	 * @access public
+	 * @param  array $ninja_form - The Ninja Forms form being converted.
+	 * @return array $form - The new Gravity Forms form object.
 	 */
 	public function convert_form( $ninja_form ) {
 
 		// Create a new Gravity Forms form object.
 		$form = array(
-			'title'         => rgar( $ninja_form, 'form_title' ), // Form title
-			'requireLogin'  => rgar( $ninja_form, 'logged_in' ), // Require login
+			'title'         => rgar( $ninja_form, 'form_title' ), // Form title.
+			'requireLogin'  => rgar( $ninja_form, 'logged_in' ), // Require login.
 			'fields'        => array(),
 			'confirmations' => array(),
 			'notifications' => array(),
 		);
-		
+
 		// Prepare fields.
 		foreach ( $ninja_form['fields'] as $_field ) {
-			
+
 			// Convert field.
 			$field = GF_Migrate_NF_Field::convert_field( $_field );
-			
+
 			// Save to fields array if converted.
 			if ( ! is_null( $field ) ) {
 				$form['fields'][ $field->id ] = $field;
 			}
-			
+
 			// If field is a submit field, push label to button form property.
 			if ( '_submit' === $_field['type'] ) {
 				$form['button'] = array(
 					'type' => 'text',
-					'text' => $_field['label']
+					'text' => $_field['label'],
 				);
 			}
-			
+
 		}
 
 		// Prepare notifications.
@@ -126,8 +245,8 @@ class GF_Migrate_NF extends GFAddOn {
 	 * Convert a Ninja Forms notification to a Gravity Forms notification/confirmation.
 	 *
 	 * @access public
-	 * @param array $form - The new Gravity Forms form object
-	 * @param array $nf_notification - The Ninja Forms notification
+	 * @param  array $form - The new Gravity Forms form object.
+	 * @param  array $nf_notification - The Ninja Forms notification.
 	 * @return array $form
 	 */
 	public function convert_notification( $form, $nf_notification ) {
@@ -183,7 +302,7 @@ class GF_Migrate_NF extends GFAddOn {
 				'replyTo'  => $this->convert_from_backticks( $form, $nf_notification['reply_to'] ),
 				'bcc'      => $this->convert_from_backticks( $form, $nf_notification['bcc'] ),
 			);
-			
+
 			// Add notification to form.
 			$form['notifications'][ $notification['id'] ] = $notification;
 
@@ -195,94 +314,94 @@ class GF_Migrate_NF extends GFAddOn {
 
 	/**
 	 * Convert Ninja Form submissions to Gravity Forms entries.
-	 * 
+	 *
 	 * @access public
-	 * @param int $ninja_form_id - The Ninja Forms form ID
-	 * @param int $ninja_form_id - The new Gravity Forms form ID
+	 * @param  int $ninja_form_id - The Ninja Forms form ID
+	 * @param  int $ninja_form_id - The new Gravity Forms form ID
 	 * @return array $entries
 	 */
 	public function convert_submissions( $ninja_form_id, $form_id = 0 ) {
-		
+
 		// Create array to story entries.
 		$entries = array();
-	
+
 		// Get submissions.
 		$submissions = GF_Migrate_NF_API::get_submissions( $ninja_form_id );
-		
+
 		// Add needed information to submissions and push to entries array.
 		if ( ! empty( $submissions ) ) {
-			
+
 			foreach ( $submissions as $entry ) {
-			
+
 				// Add missing information.
 				$entry['form_id']    = $form_id;
 				$entry['is_starred'] = 0;
 				$entry['is_read']    = 0;
 				$entry['ip']         = null;
 				$entry['user_agent'] = esc_html__( 'Ninja Forms Migration', 'migrate-ninja-forms-to-gravity-forms' );
-			
+
 				// Push to entries array.
 				$entries[] = $entry;
-				
+
 			}
 
 		}
-		
+
 		// Return entries.
 		return $entries;
-		
+
 	}
 
 	/**
 	 * Converts any Ninja Forms shortcodes in a string to Gravity Forms merge tags.
-	 * 
+	 *
 	 * @access public
-	 * @param array $form
-	 * @param string $text (default: '')
+	 * @param  array $form
+	 * @param  string $text (default: '')
 	 * @return string $text
 	 */
 	public function convert_to_merge_tags( $form, $text = '' ) {
-		
+
 		// If no text was provided, return it.
 		if ( rgblank( $text ) ) {
 			return $text;
 		}
-		
+
 		// Convert all fields shortcode.
 		$text = str_replace( '[ninja_forms_all_fields]', '{all_fields}', $text );
-		
+
 		// Search for other Ninja Forms shortcodes.
-		preg_match_all( "/(\\[ninja_forms_field id=([0-9].*)\\])/mi", $text, $matches );
-		
+		preg_match_all( '/(\\[ninja_forms_field id=([0-9].*)\\])/mi', $text, $matches );
+
 		// Loop through each shortcode match and convert to merge tags.
 		foreach ( $matches[0] as $i => $shortcode ) {
-			
+
 			// Get the field id.
 			$field_id = $matches[2][ $i ];
-			
+
 			// Make sure the field exists.
-			if ( ! isset ($form['fields'][ $field_id ] ) ) {
+			if ( ! isset ( $form['fields'][ $field_id ] ) ) {
 				continue;
 			}
-			
+
 			// Prepare merge tag.
 			$merge_tag = '{' . $form['fields'][ $field_id ]->label . ':' . $field_id . '}';
-			
+
 			// Replace shortcode.
 			$text = str_replace( $shortcode, $merge_tag, $text );
-			
+
 		}
-		
+
 		return $text;
-		
+
 	}
-	
+
 	/**
 	 * Convert backticks separated list to a comma separated list.
-	 * 
+	 *
 	 * @access public
-	 * @param array $form
-	 * @param string $text (default: '')
+	 * @param  array $form
+	 * @param  string $text (default: '')
 	 * @return string $text
 	 */
 	public function convert_from_backticks( $form, $text = '' ) {
